b75c9eae2b922aa8c08210f62ab332553af2c409
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-06 16:11:11 +0530

    Initial Commit

---
+++ src/contexts/index.js
@@ -0,0 +1,4 @@
+// humne isko issliye banaya hai taaki hum sab kuch ek baar me yhi export krde fir isse access le lenge jiska lena hoga.
+
+
+export {TodoContext, TodoProvider, useTodo} from "./TodoContext";
